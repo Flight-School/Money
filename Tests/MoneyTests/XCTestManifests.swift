2494d5d08e2569fdf44aa2f7a60b6cd70e144d62
Mattt <mattt@me.com>
2020-05-06 11:36:56 -0700

    Add EncodingTests.allTests

--- Tests/MoneyTests/XCTestManifests.swift
+++ Tests/MoneyTests/XCTestManifests.swift
@@ -5,6 +5,7 @@ public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(MoneyTests.allTests),
         testCase(DecodingTests.allTests),
+        testCase(EncodingTests.allTests),
     ]
 }
 #endif
